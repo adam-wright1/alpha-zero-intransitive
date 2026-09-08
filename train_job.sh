412c04c07705fb86a273ece006530112b270a831
adam-wright1 <wright1@stanford.edu>
2026-09-08 09:38:25 -0700

    update train_job.sh for new run

--- train_job.sh
+++ train_job.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=train_output.log
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=04:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=8G
 #SBATCH --cpus-per-task=4
 
 cd /scratch/users/wright1/alpha-zero-intransitive
 source venv/bin/activate
-python3 main.py --numIters 15 --numEps 40 --numMCTSSims 40 --arenaCompare 20 --cpuct 1.0
+python3 main.py --numIters 15 --numEps 40 --numMCTSSims 70 --arenaCompare 20 --cpuct 1.0 --load_model
