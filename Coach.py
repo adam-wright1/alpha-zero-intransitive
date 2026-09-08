6f4078d904e54e0d8a226331f057a4aac2c8ff2c
adam-wright1 <wright1@stanford.edu>
2026-09-08 12:55:01 -0700

    Altered loadTrainExamples to default to best.pth.tar if file not found

--- Coach.py
+++ Coach.py
@@ -143,15 +143,10 @@ class Coach():
         modelFile = os.path.join(self.args.load_folder_file[0], self.args.load_folder_file[1])
         examplesFile = modelFile + ".examples"
         if not os.path.isfile(examplesFile):
-            log.warning(f'File "{examplesFile}" with trainExamples not found!')
-            r = input("Continue? [y|n]")
-            if r != "y":
-                sys.exit()
+            log.warning(f'File "{examplesFile}" with trainExamples not found! Continuing without loading examples.')
         else:
             log.info("File with trainExamples found. Loading it...")
             with open(examplesFile, "rb") as f:
                 self.trainExamplesHistory = Unpickler(f).load()
             log.info('Loading done!')
-
-            # examples based on the model were already collected (loaded)
             self.skipFirstSelfPlay = True
